fc50cfcf672b0aa284fc9d3e960629bde04b4915
Thomas Chernaik <sc21trc@leeds.ac.uk>
2022-04-09 18:41:26 +0100

    written functions to import words and check if two words are adjacent

---
+++ main.py
@@ -0,0 +1,36 @@
+# In this file there will be a function to import the words list,
+# generate a graph of connected words, and convert it into a
+# javascript array for use in a website. the graph generation only
+# needs to run in polynomial time, as it is not used live.
+def importWordsToDict(fileName):
+    words = {}
+    with open(fileName, "r") as myFile:
+        words[myFile.readline()] = []
+    return words
+
+def areWordsAdjacent(word1, word2):
+    #check the length difference is not more than one
+    if abs(len(word1) - len(word2)) > 1:
+        return False
+    #if the words are the same length then we need to check that only one letter was changed:
+    if len(word1) == len(word2):
+        count = 0
+        for i in range(len(word1)):
+            count += word1[i] == word2[i]
+        if count > 1:
+            return False
+        return True
+    #make sure the longer word is first
+    if len(word1) < len(word2):
+        word1, word2 = word2, word1
+    pastDiff = False
+    for i in range(len(word1)):
+        if word1[i] != word2[i - pastDiff]:
+            if pastDiff:
+                return False
+            pastDiff = True
+    return True
+
+def getAdajacentWords(dict, word):
+    for otherWord in dict.keys():
+        if(areWordsAdjacent(word, otherWord)):
